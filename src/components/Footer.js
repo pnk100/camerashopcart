02343855063ec58b2806fea14b7bb5f599152df0
pradeep <pradeepkumargangwar8834@gmail.com>
2022-11-25 18:50:47 +0530

    All Files Uploaded

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="https://ctfvcorvbanenb7tirqgrq"
+                        className="repo_link"
+                        target=" "
+                        rel="noreferrer"
+                    >
+                        Shop address
+                    </a>
+
+                    <p>
+                        Built by | &nbsp;
+                        <a
+                            href="https://Owner.netliy.app"
+                            target=" "
+                            rel="noreferrer"
+                        >
+                            Shop Owner
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
